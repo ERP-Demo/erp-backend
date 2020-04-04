4828464c761b23225eca6b71c55b4b34cd08f4ca
41369ly <41333369@qq.com>
2020-04-04 14:45:26 +0800

    初始化

---
+++ shiying-config/src/test/java/cn/shiying/config/ErpConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.shiying.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErpConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

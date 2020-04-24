dcf0c9c5958278bab60ced2c1c454587297e392f
aaa921 <2206975657@qq.com>
2020-04-24 11:23:50 +0800

    users_department

---
+++ shiying-service-test_correlation/src/main/java/cn/shiying/test_correlation/entity/vo/TestCorrelationVO.java
@@ -0,0 +1,11 @@
+package cn.shiying.test_correlation.entity.vo;
+
+import cn.shiying.common.entity.TestProjects.TestProjects;
+import cn.shiying.common.entity.TestSynthesize.TestSynthesize;
+
+public class TestCorrelationVO {
+    //化验项目的具体内容
+    private TestProjects testProjects;
+    //化验项目的大体
+    private TestSynthesize testSynthesize;
+}

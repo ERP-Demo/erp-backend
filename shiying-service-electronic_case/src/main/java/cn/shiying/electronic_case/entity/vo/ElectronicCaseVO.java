28f61fa4c0e18b2504de1d80060e6d65c724593b
Haoran Xia <1042122451@qq.com>
2020-04-25 15:22:06 +0800

    化验项目三代目

--- shiying-service-electronic_case/src/main/java/cn/shiying/electronic_case/entity/vo/ElectronicCaseVO.java
+++ shiying-service-electronic_case/src/main/java/cn/shiying/electronic_case/entity/vo/ElectronicCaseVO.java
@@ -11,7 +11,7 @@ public class ElectronicCaseVO extends ElectronicCase {
     /**
      * 患者
      */
-    private PatientDetailed patientDetailed;
+    private String patientName;
 
 
     /**
@@ -21,5 +21,5 @@ public class ElectronicCaseVO extends ElectronicCase {
     /**
      * 科室
      */
-    private Department department;
+    private String departmentName;
 }

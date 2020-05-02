c78e19bdeb58367cf2e8ac79f62a8035cfe9ae20
aaa921 <2206975657@qq.com>
2020-05-02 13:20:38 +0800

    成药模板管理的配置

---
+++ shiying-service-drug_model/src/main/java/cn/shiying/drug_model/mapper/DrugModelMapper.java
@@ -0,0 +1,18 @@
+package cn.shiying.drug_model.mapper;
+
+import cn.shiying.drug_model.entity.DrugModel;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author tyb
+ * @since 2020-05-01
+ */
+@Mapper
+public interface DrugModelMapper extends BaseMapper<DrugModel> {
+
+}
---
+++ shiying-service-drug_model/src/main/java/cn/shiying/drug_model/service/DrugModelService.java
@@ -0,0 +1,24 @@
+package cn.shiying.drug_model.service;
+
+import cn.shiying.drug_model.entity.DrugModel;
+import com.baomidou.mybatisplus.extension.service.IService;
+import cn.shiying.common.utils.PageUtils;
+import java.util.Map;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author tyb
+ * @since 2020-05-01
+ */
+public interface DrugModelService extends IService<DrugModel> {
+
+    /**
+     * 分页查询
+     * @param params
+     * @return
+     */
+     PageUtils queryPage(Map<String, Object> params);
+}

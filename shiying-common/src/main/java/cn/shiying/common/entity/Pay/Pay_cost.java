c60b828307ad3b2786d5e769d230aefa0c7b01bb
dome <81118597@qq.com>
2020-05-15 10:26:08 +0800

    提交

--- shiying-common/src/main/java/cn/shiying/common/entity/Pay/Pay_cost.java
+++ shiying-common/src/main/java/cn/shiying/common/entity/Pay/Pay_cost.java
@@ -13,16 +13,34 @@ import lombok.Data;
  */
 @Data
 public class Pay_cost {
-    //患者
-    private PatientDetailed patientDetailed;
-    //开立项目
-    private Requirements requirements;
-    //项目
-    private TestSynthesize testSynthesize;
-    //处置
-    private PatientHandleApplyDetailed patientHandleApplyDetailed;
-    //药品
-    private Prescription prescription;
+    //患者id
+    private Integer patientId;
+    //患者姓名
+    private String patientName;
+    //就诊号
+    private String registerId;
+    //项目id
+    private Integer testSynthesizeId;
+    //项目名称
+    private String testSynthesizeName;
+    //项目价格
+    private double testSynthesizePrice;
+    //处置名称
+    private String handleName;
+    //处置价格
+    private Double handlePrice;
+    //处方名称
+    private String prescriptionName;
+    //处方价格
+    private Double prescriptionPrice;
+    //状态
+    private Integer status;
+    //药品id
+    private Integer drugsId;
+    //药品名称
+    private String drugsName;
+    //药品数量
+    private Integer drugsNum;
     //药品开立
     private PrescriptionDetails prescriptionDetails;
 }
